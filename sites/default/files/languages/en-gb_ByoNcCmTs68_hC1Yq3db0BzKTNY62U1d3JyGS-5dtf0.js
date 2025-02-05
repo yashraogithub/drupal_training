ce4fe4d6dd703b035b77b6ea4cf8ae8e0a81d899
yashraogithub <yashraobhaisare82@gmail.com>
2025-02-05 15:07:34 +0530

    add folder site

---
+++ sites/default/files/languages/en-gb_ByoNcCmTs68_hC1Yq3db0BzKTNY62U1d3JyGS-5dtf0.js
@@ -0,0 +1,1 @@
+window.drupalTranslations = {"strings":{"":{"Edit":"Edit","Status message":"Status message","Error message":"Error message","Warning message":"Warning message","Save":"Save","Cancel":"Cancel","Continue":"Continue"}},"pluralFormula":{"1":0,"default":1}};
